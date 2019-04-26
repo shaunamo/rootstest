b5aafe85d33194cd8ce5fd50f2634b0be6146516
Dan Byrne <dan@amisonltd.co.uk>
2019-04-26 13:37:49 +0100

    changes

--- site/web/app/themes/sage/resources/views/partials/header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/header.blade.php
@@ -1,12 +1,9 @@
 <header class="banner">
   <div class="container">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+    <img class="img-responsive" src="uploads/2019/04/Logo-300x128.png">
   </div>
+  
   <nav class="navbar navbar-expand-lg navbar-light bg-light text-center">
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
